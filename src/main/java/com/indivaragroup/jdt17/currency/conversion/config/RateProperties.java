193e953be4272d9f97a63ca834d9ec953b99bd85
princeofverry <verryk49@gmail.com>
2026-06-10 19:38:00 +0700

    refactor: remove setter and adding required args constructor

--- src/main/java/com/indivaragroup/jdt17/currency/conversion/config/RateProperties.java
+++ src/main/java/com/indivaragroup/jdt17/currency/conversion/config/RateProperties.java
@@ -1,6 +1,7 @@
 package com.indivaragroup.jdt17.currency.conversion.config;
 
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
@@ -8,8 +9,8 @@ import java.math.BigDecimal;
 import java.util.Map;
 
 @Getter
-@Setter
+@RequiredArgsConstructor
 @ConfigurationProperties(prefix = "currency")
 public class RateProperties {
-    private Map<String, BigDecimal> rates;
+    private final Map<String, BigDecimal> rates;
 }
